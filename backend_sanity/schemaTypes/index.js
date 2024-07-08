9d0272d377b98536c6d227673d50a36a7feb7472
Mia Dinh <miadinh38@gmail.com>
2024-07-08 10:49:25 -0400

    Add experiences, workExperience, works, and skills to scheme types

--- backend_sanity/schemaTypes/index.js
+++ backend_sanity/schemaTypes/index.js
@@ -1,11 +1,19 @@
 import abouts from "./abouts";
 import brands from "./brands";
 import contact from "./contact";
+import experiences from "./experiences";
+import skills from "./skills";
 import testimonials from "./testimonials";
+import workExperience from "./workExperience";
+import works from "./works";
 
 export const schemaTypes = [
   testimonials,
   abouts,
   brands,
-  contact
+  contact,
+  experiences,
+  workExperience,
+  works,
+  skills
 ]
