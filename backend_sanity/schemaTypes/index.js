0dc8762cb7c2b15d7517b209257606d372768847
Mia Dinh <miadinh38@gmail.com>
2024-07-08 01:05:57 -0400

    Add abouts, brands, contact and testimonials to schema types

--- backend_sanity/schemaTypes/index.js
+++ backend_sanity/schemaTypes/index.js
@@ -1,1 +1,11 @@
-export const schemaTypes = []
+import abouts from "./abouts";
+import brands from "./brands";
+import contact from "./contact";
+import testimonials from "./testimonials";
+
+export const schemaTypes = [
+  testimonials,
+  abouts,
+  brands,
+  contact
+]
