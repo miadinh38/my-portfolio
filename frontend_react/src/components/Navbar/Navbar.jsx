267cf3ef5339f99fcb8a600315a66099e9386618
Mia Dinh <miadinh38@gmail.com>
2024-07-11 16:01:41 -0400

    Init navbar component template

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -1,10 +1,24 @@
-import React from 'react';
-import './Navbar.scss'
+import React from "react";
+import "./Navbar.scss";
+
+import { images } from "../../constants/";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav className="app__navbar">
+      <div className="app_navbar-logo">
+        <img src={images.logo} alt="logo" />
+      </div>
+      <ul className="app__navbar-links">
+        {["home", "about", "work", "skills", "contact"].map((item) => (
+          <li className='app__flex p-text' key={`link-${item}`}>
+            <div />
+            <a href={`#${item}`}>{item}</a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
