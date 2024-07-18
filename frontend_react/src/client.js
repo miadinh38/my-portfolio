f2443d0c44bff4ed1369876d5a47fa0ece924b5a
Mia Dinh <miadinh38@gmail.com>
2024-07-17 22:53:53 -0400

    Fix typo

--- frontend_react/src/client.js
+++ frontend_react/src/client.js
@@ -1,15 +1,14 @@
 import { createClient }  from '@sanity/client';
 import imageUrlBuilder from '@sanity/image-url';
-import { ImageUrlBuilder } from '@sanity/image-url/lib/types/builder';
 
 export const client = createClient({
-  projectId: PROCESS.ENV.REACT_APP_SANITY_PROJECT_ID,
+  projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
   dataset: 'production',
   apiVersion: '2022-02-01',
   useCdn: true,
-  token: PROCESS.ENV.REACT_APP_SANITY_TOKEN,
+  token: process.env.REACT_APP_SANITY_TOKEN,
 });
 
-const builder = ImageUrlBuilder(client);
+const builder = imageUrlBuilder(client);
 
 export const urlFor = (source) => builder.image(source);
